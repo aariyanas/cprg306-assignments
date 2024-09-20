f7dd13364c77e2bede094a0def883214f64d47be
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-09-20 17:17:41 -0600

    week2 assgmt

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <div>
+            Aariyana Sayani
+            <p><Link href="https://github.com/aariyanas">Github link</Link></p>
+        </div>
+    )
+}
