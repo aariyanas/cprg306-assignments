184740c9e88cc0d79ceb2304cee582b4e7b2c827
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-10-09 17:48:54 -0600

    fixed intellisense settings and changed format of week3 items displaying

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,10 +3,11 @@ import React from 'react';
 export default function Item ({name, quantity, category}){
     return(
         <div>
-            <ul>
-                <li>{name}</li>
-                <li>{quantity}</li>
-                <li>{category}</li>
+            <ul className="list-disc mb-4">
+                <li>{name} x{quantity}</li>
+                <ul>
+                    <li>{category}</li>
+                </ul>
             </ul>
         </div>
     );
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,9 +1,9 @@
 import ItemList from "./item-list";
 export default function Page({name, quantity, category}){
     return (
-    <main>
-        <h1>Shopping List</h1>
-        <div>
+    <main className="bg-zinc-100">
+        <h1 className="text-2xl flex border-2 rounded-full p-10 text-lime-900 font-bold">Shopping List</h1>
+        <div className="ml-14">
             <ItemList />
         </div>
     </main>
