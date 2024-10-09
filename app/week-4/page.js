86bfe08daea7f7d6cf9cb0b58087d538617259b3
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-10-09 15:47:09 -0600

    made week4 bg prettier

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import Quantity from "./new-item";
 
 export default function Page(){
     return(
-        <main>
+        <main className="h-screen bg-zinc-50">
             <h1>Adding/Removing new items and stuff</h1>
             <Quantity />
         </main>
