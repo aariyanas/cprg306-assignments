75e66fc250325c37152eba099dec3c8c919641b4
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-10-17 11:46:08 -0600

    make form better aesthetically

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -3,7 +3,6 @@ import Quantity from "./new-item";
 export default function Page(){
     return(
         <main className="h-screen bg-zinc-50">
-            <h1>Adding/Removing new items and stuff</h1>
             <Quantity />
         </main>
     );
