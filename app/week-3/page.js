c2385440aa5b32f5e3e5496000fd11e1c2b88c14
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-10-30 13:59:20 -0600

    step 1-2 done

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,9 +1,9 @@
 import ItemList from "./item-list";
 export default function Page({name, quantity, category}){
     return (
-    <main className="bg-zinc-100">
-        <h1 className="text-2xl flex border-2 rounded-full p-10 text-lime-900 font-bold">Shopping List</h1>
-        <div className="ml-14">
+    <main className="bg-zinc-100 pt-4">
+        <h1 className="text-4xl flex border-2  border border-lime-800 rounded-full p-10 text-lime-900 font-bold justify-center bg-gradient-to-r from-amber-500 to-pink-500 mx-8">Shopping List</h1>
+        <div className="ml-20 pt-4">
             <ItemList />
         </div>
     </main>
