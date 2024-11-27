c1844c072727370bf9fb5e3782b355746e6d04af
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-11-26 21:13:25 -0700

    started stept2-3 function for fetching api

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -5,3 +5,11 @@ export default function MealIdeas(ingredient) {
     const [meals, setMeals] = useState([]);
     
 }
+
+function fetchMealIdeas(ingredient) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setMeals(data.meals);
+        });
+}
